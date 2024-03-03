d4ab7a1ef134b1b5d918552b7b30ba344cf93612
josk-mb <mao1060419504@gmail.com>
2024-03-03 14:46:54 +0800

    图标引入

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,7 +1,13 @@
+<script setup>
+
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>
-    主页界面
-    <!-- 二级路由出口 -->
-    <RouterView></RouterView> 
-  </div>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
