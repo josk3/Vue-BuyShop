d522fb247c39c987949ff71ede71be6e8b4f04e4
josk-mb <mao1060419504@gmail.com>
2024-03-04 23:28:23 +0800

    category-一级分类面包屑导航渲染

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -15,7 +15,7 @@ const { y } = useWindowScroll()
                     <RouterLink to="/">首页</RouterLink>
                 </li>
                 <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-                    <RouterLink to="/">{{ item.name }}</RouterLink>
+                    <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
                 </li>
             </ul>
             <div class="right">
--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { useCategoryStore } from '@/stores/category';
+
 const categoryStore = useCategoryStore()
 
 </script>
