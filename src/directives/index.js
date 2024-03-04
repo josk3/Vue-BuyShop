c198e6f07473a0b63138fa5178e4a48a2a346dcd
josk-mb <mao1060419504@gmail.com>
2024-03-04 15:52:11 +0800

    home-图片懒加载的优化

---
+++ src/directives/index.js
@@ -0,0 +1,32 @@
+// 定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install(app) {
+        // 自定义指令
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                // el:指令绑定的那个元素 img
+                // binding: binding.value指令等于号后面绑定的表达式的值图片url
+
+                // 测试自定义语法是否成功
+                console.log(el, binding.value)
+
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        // 测试图片监控是否生效
+                        console.log(isIntersecting)
+
+                        // 如果图片进入视口，发送 图片资源请求
+                        if (isIntersecting) {
+                            el.src = binding.value
+                            stop()
+                        }
+                    },
+                )
+
+            }
+        })
+    }
+}
