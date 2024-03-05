a9132c4818751358a5e5be63029127dd83620635
josk-mb <mao1060419504@gmail.com>
2024-03-05 20:38:17 +0800

    二级分类-路由滚动行为的优化

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,12 @@ const router = createRouter({
       path: '/login',
       component: Login
     }
-  ]
+  ],
+  scrollBehavior() {
+    return {
+      top: 0
+    }
+  }
 })
 
 export default router
