43e1902415709cf6162f8cc487f7dbd18f679a70
josk-mb <mao1060419504@gmail.com>
2024-03-05 18:19:37 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -2,8 +2,8 @@ import request from '@/utils/http'
 
 export const getTopCategoryAPI = (id) => {
     return request({
-        url : '/category',
-        params:{
+        url: '/category',
+        params: {
             id
         }
     })
@@ -17,10 +17,27 @@ export const getTopCategoryAPI = (id) => {
 
 export const getCategoryFilterAPI = (id) => {
     return request({
-      url:'/category/sub/filter',
-      params:{
-        id
-      }
+        url: '/category/sub/filter',
+        params: {
+            id
+        }
     })
-  }
-  
+}
+
+/**
+* @description: 获取导航数据
+* @data { 
+   categoryId: 1005000 ,
+   page: 1,
+   pageSize: 20,
+   sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+ } 
+* @return {*}
+*/
+export const getSubCategoryAPI = (data) => {
+    return request({
+        url: '/category/goods/temporary',
+        method: 'POST',
+        data
+    })
+}
--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,8 +1,11 @@
 <script setup>
-import {getCategoryFilterAPI} from '@/apis/category'
-import { onMounted , ref } from 'vue';
+import { getCategoryFilterAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { getSubCategoryAPI } from '@/apis/category'
+import GoodsItems from '../Home/components/GoodsItems.vue';
 
+// 获取面包屑导航数据
 const route = useRoute()
 const CategoryData = ref({})
 
@@ -11,90 +14,104 @@ const getCategoryData = async () => {
     // console.log(res)
     CategoryData.value = res.data.result
 }
-
 onMounted(() => getCategoryData())
+
+// 获取基础列表基础数据
+const reqData = ref({
+    categoryId: route.params.id,
+    page: 1,
+    pageSize: 20,
+    sortField: 'publishTime' 
+})
+const SubCategoryData = ref([])
+const getSubCategoryData = async () => {
+    const res = await getSubCategoryAPI(reqData)
+    console.log(res)
+    SubCategoryData.value = res.data.result.items
+}
+onMounted(() => getSubCategoryData())
 </script>
 
 <template>
-  <div class="container ">
-    <!-- 面包屑 -->
-    <div class="bread-container">
-      <el-breadcrumb separator=">">
-        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <!-- 路由跳转: 动态传递路由参数id -->
-        <el-breadcrumb-item :to="`/category/${CategoryData.parentId}`">{{ CategoryData.parentName }}
-        </el-breadcrumb-item>
-        <el-breadcrumb-item>{{ CategoryData.name }}</el-breadcrumb-item>
-      </el-breadcrumb>
+    <div class="container ">
+        <!-- 面包屑 -->
+        <div class="bread-container">
+            <el-breadcrumb separator=">">
+                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+                <!-- 路由跳转: 动态传递路由参数id -->
+                <el-breadcrumb-item :to="`/category/${CategoryData.parentId}`">{{ CategoryData.parentName }}
+                </el-breadcrumb-item>
+                <el-breadcrumb-item>{{ CategoryData.name }}</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="sub-container">
+            <el-tabs>
+                <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
+                <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
+                <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
+            </el-tabs>
+            <div class="body">
+                <!-- 商品列表-->
+                <GoodsItems v-for="item in SubCategoryData" :good="item" :key="item.id"></GoodsItems>
+            </div>
+        </div>
     </div>
-    <div class="sub-container">
-      <el-tabs>
-        <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
-        <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
-        <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
-      </el-tabs>
-      <div class="body">
-         <!-- 商品列表-->
-      </div>
-    </div>
-  </div>
-
 </template>
 
 
 
 <style lang="scss" scoped>
 .bread-container {
-  padding: 25px 0;
-  color: #666;
+    padding: 25px 0;
+    color: #666;
 }
 
 .sub-container {
-  padding: 20px 10px;
-  background-color: #fff;
+    padding: 20px 10px;
+    background-color: #fff;
 
-  .body {
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0 10px;
-  }
-
-  .goods-item {
-    display: block;
-    width: 220px;
-    margin-right: 20px;
-    padding: 20px 30px;
-    text-align: center;
-
-    img {
-      width: 160px;
-      height: 160px;
+    .body {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 0 10px;
     }
 
-    p {
-      padding-top: 10px;
+    .goods-item {
+        display: block;
+        width: 220px;
+        margin-right: 20px;
+        padding: 20px 30px;
+        text-align: center;
+
+        img {
+            width: 160px;
+            height: 160px;
+        }
+
+        p {
+            padding-top: 10px;
+        }
+
+        .name {
+            font-size: 16px;
+        }
+
+        .desc {
+            color: #999;
+            height: 29px;
+        }
+
+        .price {
+            color: $priceColor;
+            font-size: 20px;
+        }
     }
 
-    .name {
-      font-size: 16px;
+    .pagination-container {
+        margin-top: 20px;
+        display: flex;
+        justify-content: center;
     }
-
-    .desc {
-      color: #999;
-      height: 29px;
-    }
-
-    .price {
-      color: $priceColor;
-      font-size: 20px;
-    }
-  }
-
-  .pagination-container {
-    margin-top: 20px;
-    display: flex;
-    justify-content: center;
-  }
 
 
 }
